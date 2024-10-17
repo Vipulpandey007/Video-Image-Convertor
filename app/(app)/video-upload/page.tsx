a4c8ce17c336a4b0e1df0e571647b2b1eff475e5
Vipulpandey007 <vipulpandey708@gmail.com>
2024-10-17 11:16:49 +0530

    prisma and neondb connected

---
+++ app/(app)/video-upload/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function page() {
+  return <div>Videoupload</div>;
+}
+
+export default page;
--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,35 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher([
+  "/sign-in",
+  "/sign-up",
+  "/",
+  "/home",
+]);
+
+const isPublicApiRoute = createRouteMatcher(["/api/videos"]);
+
+export default clerkMiddleware((auth, req) => {
+  const { userId } = auth();
+  const currentUrl = new URL(req.url);
+  const isHomePage = currentUrl.pathname === "/home";
+  const isApiRequest = currentUrl.pathname.startsWith("/api");
+
+  if (userId && isPublicRoute(req) && !isHomePage) {
+    return NextResponse.redirect(new URL("/home", req.url));
+  }
+
+  if (!userId) {
+    if (!isPublicRoute(req) && !isPublicApiRoute(req)) {
+      return NextResponse.redirect(new URL("/sign-in", req.url));
+    }
+    if (isApiRequest && !isPublicApiRoute(req)) {
+      return NextResponse.redirect(new URL("/sign-in", req.url));
+    }
+  }
+  return NextResponse.next();
+});
 
 export const config = {
   matcher: [
